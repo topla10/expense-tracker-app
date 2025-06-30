0bcad71aa407bd80149980b42aaa7dde0a0a4ac1
Maxime <gilotmaxime@outlook.com>
2025-06-30 05:53:57 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const description = document.querySelector('#description');
+const amount = document.querySelector('#amount');
+const category = document.querySelector('#category');
+const form = document.querySelector('form');
+const expenseList = document.querySelector('ul');
+const filterByCategory = document.querySelector('#filterCategory');
+const total = document.querySelector('#total');
+const filterBtn = document.querySelector('#filterBtn');
+
+const listItemArray = [];
+
+form.addEventListener('submit', handleInput);
+
+function handleInput(event) {
+	event.preventDefault();
+
+	handleListItem();
+	clearInput();
+}
+
+function handleListItem() {
+	const newItem = createItem();
+	addExpense(newItem);
+	renderList(listItemArray);
+	totalCalc();
+}
+
+function createItem() {
+	const listItemObj = {
+		description: description.value,
+		amount: parseFloat(amount.value),
+		category: category.value,
+	};
+	return listItemObj;
+}
+
+function addExpense(newItem) {
+	listItemArray.push(newItem);
+}
+
+function renderList(arr) {
+	expenseList.innerHTML = '';
+	arr.forEach((element, index) => {
+		const liElement = document.createElement('li');
+		const spanElement = document.createElement('span');
+		const deleteBtn = document.createElement('button');
+		deleteBtn.className = 'delete';
+		spanElement.textContent = `${element.description} (${element.category}): $${element.amount}`;
+		deleteBtn.textContent = 'delete';
+		deleteBtn.addEventListener('click', () => {
+			arr.splice(index, 1);
+			renderList(arr);
+			totalCalc();
+		});
+		liElement.appendChild(spanElement);
+		liElement.appendChild(deleteBtn);
+		expenseList.appendChild(liElement);
+	});
+}
+const clearInput = function () {
+	description.value = '';
+	category.value = '';
+	amount.value = '';
+};
+
+function totalCalc() {
+	let totalPrice = 0;
+
+	for (const i of listItemArray) {
+		totalPrice += i.amount;
+	}
+	total.innerHTML = totalPrice.toFixed(2);
+}
